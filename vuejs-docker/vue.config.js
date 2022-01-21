0bfb117fbaaa09d7b5293da1b16c9f7dae2e79da
nimon <nimon@42paris.fr>
2022-01-21 21:08:51 +0100

    Change nuxt to Vue and postgre to pgadmin

---
+++ vuejs-docker/vue.config.js
@@ -0,0 +1,11 @@
+// module.exports = {
+// 	configureWebpack: {
+// 	  devServer: {
+// 		port: 8081,
+// 		// https://github.com/vuejs-templates/webpack/issues/378
+// 		watchOptions: {
+// 		  poll: true,
+// 		},
+// 	  },
+// 	}
+//   };
