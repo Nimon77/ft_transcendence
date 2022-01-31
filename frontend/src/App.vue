9455bd387c57502d1f63c67786ef067357e3c5ee
nimon <nimon@42paris.fr>
2022-01-31 08:05:17 +0100

    add vue router module

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
-</script>
 
 <style>
 #app {
@@ -22,6 +13,18 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	devServer: {
-	  // Fixing issue with WDS disconnected and sockjs network error
-	  host: '0.0.0.0',
-	  public: '0.0.0.0:8080',
-	  disableHostCheck: true,
-	  // End of fix
-	  progress: false
+		// Fixing issue with WDS disconnected and sockjs network error
+		host: '0.0.0.0',
+		public: '0.0.0.0:8080',
+		disableHostCheck: true,
+		// End of fix
+		progress: false
 	}
-  }
+}
