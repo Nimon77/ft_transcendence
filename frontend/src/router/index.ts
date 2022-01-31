762c585e7cf32a54736250e5f4dc44bc884ad7a2
Melchior MAJ <mmaj@e1r9p9.42.fr>
2022-01-31 16:56:26 +0100

    begin project

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+import App from '../App.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'App',
+    component: App
   },
   {
     path: '/about',
