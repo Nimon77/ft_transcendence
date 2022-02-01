630a300548fd56bbcdf251fc42d3323ebaf49a3f
mmaj0708 <djoebul@gmail.com>
2022-02-01 19:10:57 +0100

    begin header + buttons main page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,57 +1,29 @@
-
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar app elevation="0" color="green">
+      <h1 style="color: white">BananaPong</h1>
     </v-app-bar>
 
-    <v-main>
-      <router-view/>
+    <v-main >
+      <Twobuttons style="margin-top: 200px;"/>
     </v-main>
+
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+  import Twobuttons from './components/Twobuttons.vue'
 
-export default Vue.extend({
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-});
+  export default {
+    components: {
+      Twobuttons,
+    },
+    data: () => {
+      return {
+        drawer: null
+      }
+    },
+  };
 </script>
+
---
+++ frontend/src/components/Twobuttons.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <v-container fluid class="ctn">
+    <v-row style="justify-content: center;">
+      <v-col cols="2" >
+        <p> <v-btn color="primary" tile large depressed width="175" height="75"> PLAY </v-btn> </p>
+        <p> <v-btn color="primary" tile large depressed width="175" height="75"> COMMUNITY </v-btn> </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Twobuttons',
+    data: () => ({
+
+    }),
+  }
+</script>
+
+<style scoped>
+.ctn { max-width: 800px; }
+p + p { margin-top: 10px; }
+</style>
