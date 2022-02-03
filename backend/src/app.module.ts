c94d7fad9926efd8ef012195503a264c355a8b62
adam <adjemaa@student.42.fr>
2022-02-03 22:15:00 +0100

    photo entity and module for avatar, friends list wip

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './infrastructure/database.module';
+import { PhotoModule } from './photo/photo.module';
 import { UserModule } from './user/user.module';
 import * as Joi from '@hapi/joi';
 
@@ -16,7 +17,7 @@ import * as Joi from '@hapi/joi';
         POSTGRES_DB: Joi.string().required(),
         PORT: Joi.number(),
       })
-    }), DatabaseModule, UserModule
+    }), DatabaseModule, UserModule, PhotoModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/infrastructure/user.entity.ts
+++ backend/src/infrastructure/user.entity.ts
@@ -1,4 +1,5 @@
 import { Column, Entity, JoinColumn, PrimaryGeneratedColumn, OneToOne } from 'typeorm';
+import { Photo } from 'src/photo/photo.entity';
  
 @Entity()
 export class User {
@@ -14,6 +15,21 @@ export class User {
   @Column()
   public rank: number;
 
+  @Column('int', {array: true})
+  public friends: number[];
+
+  @Column('int', {array: true})
+  public blocked: number[];
+
+  @JoinColumn({ name: 'avatarId'})
+  @OneToOne(
+    () => Photo,
+    {
+      nullable: true
+    }
+  )
+
+  public avatar?: Photo;
   @Column({ nullable: true })
   public avatarId: number;
 }
