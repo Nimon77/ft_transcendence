fea7027cad82a990241c61dc1949c83321668f75
Assxios <droge@student.42.fr>
2022-02-09 16:23:19 +0100

    update: imports for db added, ready to save user in db

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { FortyTwoStrategy } from './strategies/42.strategy';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
-    PassportModule,
+    PassportModule, UserModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '7d' },
