046d7a12c80eab64b410c5a91254a71c444d6af4
Assxios <droge@student.42.fr>
2022-02-09 16:23:19 +0100

    update: imports for db added, ready to save user in db

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(private readonly jwtService: JwtService, private readonly userService: UserService) {}
 
   async login(user: any) {
+    //console.log(user);
+    //this.userService.createUser(user);
     const payload = { sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
