a503bfa842ec976a27dcb80ccdd1e30211bf5fc0
mmaj0708 <djoebul@gmail.com>
2022-02-10 15:36:50 +0100

    keep on community page

--- frontend/src/components/community/Community.vue
+++ frontend/src/components/community/Community.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-    <v-navigation-drawer v-model="drawer" width="30%" app clipped>
+    <v-navigation-drawer v-model="drawer" width="25%" app clipped>
       <v-sheet color="green" dark height="100" width="100%" class="text-center">
       <v-divider class="pt-4"></v-divider>
       <span class="span"> CHANNELS </span>
@@ -14,7 +14,7 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-navigation-drawer app clipped right width="30%">
+    <v-navigation-drawer app clipped right width="25%">
       <v-sheet color="green" height="100" dark width="100%" class="text-center">
       <v-divider class="pt-4"></v-divider>
       <span class="span"> PLAYERS </span>
@@ -28,16 +28,37 @@
       </v-list>
     </v-navigation-drawer>
 
- 
+    
+      <v-card class="mx-auto" color="white" max-width="680" height="680">
+        <v-card-title class="text-center">
+          GNE
+        </v-card-title>
+        <v-card-actions>
+        <v-app-bar bottom color="rgba(0,0,0,0)" flat>
+              <v-text-field
+              
+              v-model="message"
+              append-outer-icon="mdi-send"
+              filled
+              clear-icon="mdi-close-circle"
+              label="Message"
+              type="text"
+            ></v-text-field>
+        </v-app-bar>
+      </v-card-actions>
+      </v-card>
+  
+      <!-- <v-card class="mt-10 mr-2" max-width="350px" color="blue" dark>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <div class=" mb-4">
+              GNE
+            </div>
+            <v-list-item-subtitle>2 mins ago <span class="ml-16">Seen 1:23PM</span></v-list-item-subtitle>  
+          </v-list-item-content>
+        </v-list-item>
+      </v-card > -->
 
-      <v-text-field
-        background-color="grey lighten-1"
-        dense
-        flat
-        hide-details
-        rounded
-        solo
-      ></v-text-field>
 </div>
 </template>
 
