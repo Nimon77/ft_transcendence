ce2f09facc0c374ac7238bcdd8f59bd3c82c4ff3
Assxios <droge@student.42.fr>
2022-02-10 17:02:22 +0100

    update: basic user is created on db

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
+import { User } from '../infrastructure/user.entity';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly jwtService: JwtService, private readonly userService: UserService) {}
 
   async login(user: any) {
-    //console.log(user);
-    //this.userService.createUser(user);
+    const user1 = new User();
+    user1.log = user.username;
+    user1.id = user.id;
+    this.userService.createUser(user1);
     const payload = { sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
