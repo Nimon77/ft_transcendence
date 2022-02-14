488c00c3f28c3f8ee23cf2a30cfaa4d354968d54
Melchior MAJ <mmaj@e2r11p9.clusters.42paris.fr>
2022-02-14 17:55:07 +0100

    fix things

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 // import Home from '../views/Home.vue'
-import App from '../App.vue'
+// import App from '../App.vue'
 import Community from '../components/community/Community.vue'
 import ProfilPlayer from '../components/profilPlayer/ProfilPlayer.vue'
 import Pregame from '../components/game/Pregame.vue'
@@ -14,7 +14,7 @@ const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'App',
-    component: App
+    component: Main
   },
   {
     path: '/community',
@@ -27,45 +27,26 @@ const routes: Array<RouteConfig> = [
     component: Main
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/profilPlayer',
+    name: 'profilPlayer',
+    component: ProfilPlayer
+  },
+  {
+    path: '/preGame',
+    name: 'preGame',
+    component: Pregame
+  },
+  {
+    path: '/game',
+    name: 'game',
+    component: Game,
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes : [
-    {
-      path: '/community',
-      name: 'community',
-      component: Community
-    },
-    {
-      path: '/main',
-      name: 'main',
-      component: Main
-    },
-    {
-      path: '/profilPlayer',
-      name: 'profilPlayer',
-      component: ProfilPlayer
-    },
-    {
-      path: '/preGame',
-      name: 'preGame',
-      component: Pregame
-    },
-    {
-      path: '/game',
-      name: 'game',
-      component: Game,
-    },
-  ]
+  routes,
 })
 
 export default router
