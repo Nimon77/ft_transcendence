dda867bc58eee085f1f8eed21ef7da34f624222a
mmaj0708 <djoebul@gmail.com>
2022-02-15 10:31:03 +0100

    testing get/post

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import axios from 'axios'
 
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
