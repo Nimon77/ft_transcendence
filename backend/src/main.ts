a235dff29c304a3fe753084684e3c2ce4f2a5d0d
nimon <nimon@42paris.fr>
2022-02-15 21:38:35 +0100

    Update backend to use CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: {
+    origin: 'http://127.0.0.1:8080',
+    credentials: true,
+  }});
   const configService: ConfigService = app.get(ConfigService);
   const config = new DocumentBuilder()
     .setTitle('Cats example')
