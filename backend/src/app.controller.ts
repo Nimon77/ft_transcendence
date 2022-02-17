e5f1a845fd02f9d940276d2ef34401f251e44747
Alexandre Hallaine <hallainealexandre@gmail.com>
2022-02-17 20:02:06 +0100

    add: back global jwt

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,14 +1,21 @@
 import { Controller, Get, Post, Request, UseGuards, Res } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
+import { Public } from './auth/decorators/public.decorator';
 import { FortyTwoAuthGuard } from './auth/guards/42-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @UseGuards(FortyTwoAuthGuard)
   @Get('auth/42/callback')
   async login(@Request() req, @Res() res) {
-    res.status(302).redirect('http://127.0.0.1:8080/login?code=' + (await this.authService.login(req.user)).access_token);
+    res
+      .status(302)
+      .redirect(
+        'http://127.0.0.1:8080/login?code=' +
+          (await this.authService.login(req.user)).access_token,
+      );
   }
 }
