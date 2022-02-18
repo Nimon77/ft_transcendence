37783a1542a396d1e963f612abdd83ec528c9268
Alexandre Hallaine <hallainealexandre@gmail.com>
2022-02-18 15:40:18 +0100

    update: swagger

--- backend/src/photo/photo.module.ts
+++ backend/src/photo/photo.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PhotoService } from 'src/photo/photo.service';
 import { Photo } from './photo.entity';
-import { PhotoController } from './photo.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Photo])],
   providers: [PhotoService],
-  controllers: [PhotoController],
   exports: [PhotoService],
 })
 export class PhotoModule {}
