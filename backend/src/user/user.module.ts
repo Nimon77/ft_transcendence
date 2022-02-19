36f6f94b7dc2045a5e0638416e2469a50ef83490
adam <adjemaa@student.42.fr>
2022-02-19 22:06:28 +0100

    added room post + get for user

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../infrastructure/user.entity';
 import { PhotoService } from 'src/photo/photo.service';
 import { PhotoModule } from 'src/photo/photo.module';
+import { ChatModule } from 'src/chat/chat.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), PhotoModule],
+  imports: [TypeOrmModule.forFeature([User]), PhotoModule, ChatModule],
   providers: [UserService],
   controllers: [UserController],
   exports: [UserService],
