65e0e8f6c159cdc966c0f8c349b0aa78d5b7f1e3
mmaj0708 <djoebul@gmail.com>
2022-02-21 17:33:49 +0100

    change unique username

--- frontend/src/components/header/settingsHeader/ChangeAvatar.vue
+++ frontend/src/components/header/settingsHeader/ChangeAvatar.vue
@@ -5,16 +5,22 @@
 
     <v-dialog transition="scale-transition" max-width="400" persistent>
         <template v-slot:activator="{ on, attrs }">
-          <v-btn v-bind="attrs" v-on="on" v-on:click="test" color="blue" dark max-width="160"> Avatar/Username </v-btn>
+          <v-btn v-bind="attrs" v-on="on" color="blue" dark max-width="160"> Avatar/Username </v-btn>
         </template>
         <template v-slot:default="dialog">
           <v-card>
-            <v-toolbar color="primary" dark>CHANGE YOUR THINGS HERE</v-toolbar>
+            <v-toolbar color="primary" elevation="0" class="text-h5 pl-5" dark>CHANGE YOUR THINGS HERE</v-toolbar>
             <v-card-text>
               <v-container fluid>
               <v-row>
-              <v-col align="center" cols="12" class="mt-10" >
-              <v-text-field label="new username" solo></v-text-field>
+              <v-col align="center" cols="12" class="mt-11" >
+                
+                
+                <v-form ref="form" v-model="valid">
+                <v-text-field v-model="username" :rules="rules" label="new username" solo></v-text-field>
+                </v-form>
+              
+              
               </v-col>
               <v-col cols="12">
                 <v-file-input label="new avatar" chips truncate-length="21" ></v-file-input>
@@ -22,10 +28,9 @@
               </v-row>
               </v-container>
             </v-card-text>
-            <!-- <v-img class="ml-16" width="60"  src="https://upload.wikimedia.org/wikipedia/commons/5/56/Paloma_brav%C3%ADa_%28Columba_livia%29%2C_Palacio_de_Nymphenburg%2C_M%C3%BAnich%2C_Alemania01.JPG">
-            </v-img> -->
             <v-card-actions class="justify-end">
-              <v-btn @click="dialog.value = false" >OK!</v-btn>
+              <v-btn color="red" dark @click="dialog.value = false" >CANCEL</v-btn>
+              <v-btn :disabled="!valid" @click="setNewName" >OK!</v-btn>
             </v-card-actions>
           </v-card>
         </template>
@@ -38,18 +43,63 @@ import Vue from 'vue';
 export default Vue.extend({
         name: 'ChangeAvatar',
         components: {
-
         },
         data(): unknown {
             return {
-
+              isFound: false,
+              users: [],
+              valid: true,
+              username: '',
             }
         },
         methods: {
-            test(): void {
-                console.log('CHANGE AVATAR');
-            } 
+          async setNewName() {
+            this.dialog = false;
+            await this.$http.put('/user/me', {
+              username: this.username,
+            }).then(response => {
+              console.log('PUT REQUEST', response);
+              });
+            location.reload();
+          }
+        },
+
+        async created() {
+          await this.$http.get('/user').then(response => {
+            this.users = response.data;
+          });
+            console.log('USERS : ', this.users);
+        },
+        computed: {
+          rules() {
+            const rules = [];
+            let existingName: string = null;
+            let i = 0;
+            // console.log('IN RULES, username = ', this.username);
+
+            existingName = this.users[i].log;
+            while (this.username != existingName && i < this.users.length) {
+              // console.log('EXISTING NAME : ', existingName);
+              existingName = this.users[i++].log;
+              // i++;
+            }
+            // console.log('AFTER WHILE ', existingName);
+            if (this.username === existingName) {
+              const rule = `username already exist`;
+              rules.push(rule);
+            }
+            let rule = v => !!v;
+            rules.push(rule);
+            let rule2 = v => (v && v.length <= 8) || 'must be less than 8 characters';
+            rules.push(rule2);
+            return rules;
+          }
         }
+        // watch: {
+        //   username: function() {
+        //     console.log('CHECKING USERNAME')
+        //   },
+        // },
     });
 </script>
 
