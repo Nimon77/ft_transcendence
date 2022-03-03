34be75e790725653a94584e7d7014b272945071e
nimon <nimon@42paris.fr>
2022-03-03 09:38:50 +0100

    add cropper and send to change avatar

--- frontend/src/components/header/settingsHeader/ChangeAvatar.vue
+++ frontend/src/components/header/settingsHeader/ChangeAvatar.vue
@@ -17,7 +17,23 @@
               
               </v-col>
               <v-col cols="12">
-                <v-file-input label="new avatar" chips truncate-length="21" ></v-file-input>
+                <v-card class="mt-6" v-if="image.src">
+                  <cropper
+                    :src="image.src"
+                    :stencil-props="{
+                      aspectRatio: 1,
+                    }"
+                    ref="cropper"
+                  />
+                </v-card>
+              </v-col>
+              </v-row>
+              <v-row>
+              <v-col cols="12" class="text-center">
+                <v-btn @click="$refs.file.click()">
+                  <v-icon>mdi-upload</v-icon> Upload Avatar
+                  <input ref="file" type="file" accept="image/*" style="display:none" @change="loadImage($event)" />
+                </v-btn>
               </v-col>
               </v-row>
               </v-container>
@@ -33,6 +49,33 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import { Cropper } from 'vue-advanced-cropper';
+import 'vue-advanced-cropper/dist/style.css';
+
+Vue.component('cropper', Cropper);
+
+// This function is used to detect the actual image type,
+function getMimeType(file, fallback = null) {
+  const byteArray = new Uint8Array(file).subarray(0, 4);
+  let header = '';
+  for (let i = 0; i < byteArray.length; i++) {
+    header += byteArray[i].toString(16);
+  }
+  switch (header) {
+    case '89504e47':
+      return 'image/png';
+    case '47494638':
+      return 'image/gif';
+    case 'ffd8ffe0':
+    case 'ffd8ffe1':
+    case 'ffd8ffe2':
+    case 'ffd8ffe3':
+    case 'ffd8ffe8':
+      return 'image/jpeg';
+    default:
+      return fallback;
+  }
+}
 
 export default Vue.extend({
         name: 'ChangeAvatar',
@@ -44,16 +87,46 @@ export default Vue.extend({
               users: [],
               valid: true,
               username: '',
+              image: {
+                src: '',
+                type: '',
+              },
             }
         },
         methods: {
           async setNewName() {
             this.dialog = false;
+            const { canvas } = this.$refs.cropper.getResult();
+            if (canvas) {
+              canvas.toBlob((blob) => {
+                const form = new FormData();
+                form.append('file', blob);
+                this.$http.put('/user/me/avatar', form);
+              }, this.image.type);
+            }
             await this.$http.put('/user/me', {username: this.username,}).then(response => {
               console.log('PUT REQUEST', response);
               });
             location.reload();
-          }
+          },
+
+          loadImage(event) {
+            const { files } = event.target;
+            if (files && files[0]) {
+              if (this.image.src) {
+                URL.revokeObjectURL(this.image.src);
+              }
+              const blob = URL.createObjectURL(files[0]);
+              const reader = new FileReader();
+              reader.onload = (e => {
+                this.image = {
+                  src: blob,
+                  type: getMimeType(e.target.result, files[0].type),
+                };
+              });
+              reader.readAsArrayBuffer(files[0]);
+            }
+          },
         },
 
         async created() {
