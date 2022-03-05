3b42a1c559db56ef890dfef119c81f7dafdb1804
Hallaine Alexandre <hallainealexandre@gmail.com>
2022-03-05 18:41:15 +0100

    back fix: user create delete

--- backend/src/user/user.entity.ts
+++ backend/src/user/user.entity.ts
@@ -1,41 +1,44 @@
-import { Column,PrimaryColumn, Entity, JoinColumn, PrimaryGeneratedColumn, OneToOne, ManyToMany } from 'typeorm';
+import {
+  Column,
+  PrimaryColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  ManyToMany,
+} from 'typeorm';
 import { Avatar } from 'src/user/avatar/avatar.entity';
 import { ChatRoom } from 'src/chat/chat.entity';
- 
+
 @Entity()
 export class User {
   @PrimaryColumn()
   public id: number;
- 
-  @Column({nullable: true, unique: true})
+
+  @Column({ nullable: true, unique: true })
   public username: string;
 
-  @Column({default: 0})
+  @Column({ default: 0 })
   public rank: number;
 
-  @Column({default: ""})
+  @Column({ nullable: true })
   public otp: string;
 
-  @Column('int', {array: true, default: []})
+  @Column('int', { array: true, default: [] })
   public friends: number[];
 
-  @Column('int', {array: true, default: []})
+  @Column('int', { array: true, default: [] })
   public blocked: number[];
 
-  @Column('boolean', {default: false})
+  @Column('boolean', { default: false })
   public profileCompleted: boolean;
 
-  @ManyToMany(() => ChatRoom, room => room.users)
+  @ManyToMany(() => ChatRoom, (room) => room.users)
   rooms: ChatRoom[];
 
-  @JoinColumn({ name: 'avatarId'})
-  @OneToOne(
-    () => Avatar,
-    {
-      nullable: true
-    }
-  )
-
+  @JoinColumn({ name: 'avatarId' })
+  @OneToOne(() => Avatar, {
+    nullable: true,
+  })
   public avatar?: Avatar;
   @Column({ nullable: true })
   public avatarId: number;
