98d66bdd85b4b0b45bdc980eceee68f7822689b6
Assxios <droge@student.42.fr>
2022-03-07 19:10:26 +0100

    Update: removed useless interfaces and   continued notify socket

--- backend/src/user/interfaces/user.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  id: number;
-  username?: string;
-  onlineStatus?: boolean;
-  profileCompleted?: boolean;
-  rank?: number;
-  friends?: number[];
-  blocked?: number[];
-}
