0b0abbcf2fe743b4d70fc96813517e2c893f9baf
nimon <nimon@42paris.fr>
2022-03-09 18:32:31 +0100

    add Vuex (really this time)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,15 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import vuetify from './plugins/vuetify';
 import axios from 'axios';
 import VueAuthImage from 'vue-auth-image';
 import VueAxios from 'vue-axios';
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+import App from './App.vue';
+import router from './router';
+import vuetify from './plugins/vuetify';
+import Component from 'vue-class-component';
+import store from './store'
 
 Vue.prototype.$http = axios;
 // Vue.prototype.$http = axios
@@ -12,6 +17,11 @@ Vue.config.productionTip = false;
 
 Vue.use(VueAxios, axios);
 Vue.use(VueAuthImage);
+Vue.use(Toast, {
+  transition: "Vue-Toastification__bounce",
+  maxToasts: 20,
+  newestOnTop: true
+});
 
 // put to uppercase
 Vue.filter("upCase", value => {
@@ -25,6 +35,7 @@ axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getIte
 new Vue({
   router,
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app');
 
