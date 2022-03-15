431eb46ed348d85630201594e5d04a68022431ea
mmaj0708 <djoebul@gmail.com>
2022-03-15 13:31:57 +0100

    preGame error

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -10,6 +10,7 @@ import Game from '@/views/Game.vue'
 import Main from '@/views/Main.vue'
 import UpdateProfile from '@/views/UpdateProfile.vue'
 import store from '@/store'
+import { Socket } from 'socket.io-client'
 
 Vue.use(VueRouter)
 
@@ -136,9 +137,20 @@ router.beforeEach((to, from, next) => {
 
     // mmaj
     // prevent user to get manually to pregame and game
-    // créer le game sock dans vuex
-    // if (to.name === 'pregame' && store.getters.gameSock == undefined)
-    //   return next({ name: 'Main'});
+    console.log("check gameROOM", store.state.gameRoom.length);
+    if (to.name === 'preGame' && !store.state.gameRoom.length) {
+      console.log("to PREGAME");
+      return next({ name: 'Main'});
+    }
+    if (from.name === 'preGame' && store.state.gameRoom.length) {
+      alert("you are leaving the room!");
+      store.state.gameSock.on('disconnect', ()=>{
+        console.log("disconnected");
+        store.commit('setGameRoom', '');
+        store.state.gameSock.disconnect();
+        store.state.gameSock.close();});
+        store.state.gameSock.disconnect(true);
+      }
     next();
   });
 })
