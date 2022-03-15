a4b49e384a30f34956241514c43fcbfe9d167256
Hallaine Alexandre <hallainealexandre@gmail.com>
2022-03-15 21:00:54 +0100

    back update: send messagbe

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -4,11 +4,12 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket, Server } from 'socket.io';
+import { Socket } from 'socket.io';
 import { AuthService } from 'src/auth/auth.service';
 import { User } from 'src/user/user.entity';
 import { UserService } from 'src/user/user.service';
 import { ChatService } from './chat.service';
+import { ChatRoom } from './entity/chat.entity';
 
 @WebSocketGateway({
   cors: {
@@ -23,7 +24,7 @@ export class ChatGateway implements OnGatewayConnection {
     private readonly chatService: ChatService,
   ) {}
   @WebSocketServer()
-  server: Server;
+  server: any;
 
   async handleConnection(client: Socket) {
     if (!client.handshake.headers.authorization) return client.disconnect();
@@ -44,17 +45,28 @@ export class ChatGateway implements OnGatewayConnection {
 
   @SubscribeMessage('channel')
   async joinChannel(client: Socket, id: number) {
-    const channel = await this.chatService.getRoom(id, ['users', 'muted', 'banned', 'logs']);
+    const channel = await this.chatService.getRoom(id, [
+      'users',
+      'muted',
+      'banned',
+      'logs',
+    ]);
     client.emit('channel', channel);
   }
 
   @SubscribeMessage('text')
-  async handleMessage(client: Socket, data: object) {
+  async handleMessage(client: Socket, data: any) {
+    const sockets: any[] = Array.from(this.server.sockets.values());
+    const room = await this.chatService.getRoom(data.id, ['users']);
+
     const user = client.data.user;
 
-    this.server.emit('text', {
-      user: { id: user.id, username: user.username },
-      ...data,
+    sockets.forEach((socket) => {
+      if (room.users.find((user) => user.id == socket.data.user.id))
+        socket.emit('text', {
+          user: { id: user.id, username: user.username },
+          ...data,
+        });
     });
   }
 }
