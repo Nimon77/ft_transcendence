e8c8dca5b309555d352de8df1e6521278a598ba3
nimon <nimon@42paris.fr>
2022-03-15 11:28:50 +0100

    refix clean.sh

--- clean.sh
+++ clean.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+ECHO=echo
 if [[ "$OSTYPE" != "darwin"* ]]
 then
-  alias echo="echo -e"
+  ECHO="echo -e"
 fi
 
 if [[ $1 == "bdd" ]]
@@ -14,21 +15,21 @@ then
 	docker-compose down && \
 	docker volume prune -f
 	if [ $? -eq 0 ]; then
-		echo "${GREEN}Cleanup success${NC}"
+		${ECHO} "${GREEN}Cleanup success${NC}"
 	else
-		echo "${RED}Cleanup failed${NC}"
+		${ECHO} "${RED}Cleanup failed${NC}"
 	fi
 elif [[ $1 == "all" ]]
 then
 	docker-compose down && \
 	docker volume prune -f && \
 	docker network prune -f && \
-	rm -rf */node_modules */*lock*
+	sudo rm -rf */node_modules */*lock*
 	if [ $? -eq 0 ]; then
-		echo "${GREEN}Cleanup success${NC}"
+		${ECHO} "${GREEN}Cleanup success${NC}"
 	else
-		echo "${RED}Cleanup failed${NC}"
+		${ECHO} "${RED}Cleanup failed${NC}"
 	fi
 else
-	echo "Usage: ./clean.sh [bdd|all]"
+	${ECHO} "Usage: ./clean.sh [bdd|all]"
 fi
