7c62f22186c75bdd470a5add10722dd5721cd1be
Hallaine Alexandre <hallainealexandre@gmail.com>
2022-03-21 04:39:56 +0100

    back add: otp

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as https from 'https';
 import { User } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/services/user.service';
 import { FortyTwoUser } from './interfaces/42user.interface';
+import { generateSecret, verify } from '2fa-util';
 
 const download = (url: string): Promise<Buffer> =>
   new Promise((resolve, reject) => {
@@ -19,6 +20,8 @@ const download = (url: string): Promise<Buffer> =>
 
 @Injectable()
 export class AuthService {
+  private secrets: Map<number, string> = new Map();
+
   constructor(
     private readonly jwtService: JwtService,
     private readonly userService: UserService,
@@ -45,5 +48,38 @@ export class AuthService {
     } catch {
       return {};
     }
+  }
+
+  async generateQR(userId: number): Promise<string> {
+    const user = await this.userService.getUser(userId, []);
+    if (user.otp)
+      throw new HttpException('Already setup', HttpStatus.FORBIDDEN);
+
+    const output = await generateSecret(user.username, 'BananaPong');
+    this.secrets.set(user.id, output.secret);
+    return output.qrcode;
+  }
+
+  async verifyCode(
+    userId: number,
+    code: string,
+    secret?: string,
+  ): Promise<void> {
+    const user = await this.userService.getUser(userId, []);
+    if (!secret) secret = user.otp;
+
+    if (!secret)
+      throw new HttpException('No secret found', HttpStatus.NOT_FOUND);
+
+    const check = await verify(code, secret);
+    if (!check) throw new HttpException('Invalid code', HttpStatus.FORBIDDEN);
+  }
+
+  async saveSecret(userId: number, code: string): Promise<void> {
+    const secret = this.secrets.get(Number(userId));
+    await this.verifyCode(userId, code, secret);
+
+    this.secrets.delete(userId);
+    await this.userService.updateOTP(userId, secret);
   }
 }
