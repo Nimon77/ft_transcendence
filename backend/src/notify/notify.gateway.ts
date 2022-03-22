676d5c8dfd6306f4b10a81d3653c9984ad7d7f87
Assxios <droge@student.42.fr>
2022-03-22 03:38:09 +0100

    Update: error checking on EVERY gateaway

--- backend/src/notify/notify.gateway.ts
+++ backend/src/notify/notify.gateway.ts
@@ -29,46 +29,58 @@ export class NotifyGateway {
   }
 
   async handleConnection(client: Socket): Promise<any> {
-    const user = await this.authService.getUserFromSocket(client);
-    if (!user) return client.disconnect();
+    try {
+      const user = await this.authService.getUserFromSocket(client);
+      if (!user) return client.disconnect();
 
-    await this.userService.setStatus(user.id, Status.ONLINE);
+      await this.userService.setStatus(user.id, Status.ONLINE);
 
-    client.data.user = user;
+      client.data.user = user;
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   handleDisconnect(client: Socket): void {
-    if (!client.data.user) return;
+    try {
+      if (!client.data.user) return;
 
-    const userId = client.data.user.id;
+      const userId = client.data.user.id;
 
-    setTimeout(async () => {
-      const socket: any = Array.from(this.server.sockets.values()).find(
-        (socket: Socket) => socket.data.user.id == userId,
-      );
-      if (socket) return;
+      setTimeout(async () => {
+        const socket: any = Array.from(this.server.sockets.values()).find(
+          (socket: Socket) => socket.data.user.id == userId,
+        );
+        if (socket) return;
 
-      const user = await this.userService.getUser(userId, []);
-      if (!user) return;
+        const user = await this.userService.getUser(userId, []);
+        if (!user) return;
 
-      if (user.status == Status.ONLINE)
-        await this.userService.setStatus(user.id, Status.OFFLINE);
-    }, 5 * 1000);
+        if (user.status == Status.ONLINE)
+          await this.userService.setStatus(user.id, Status.OFFLINE);
+      }, 5 * 1000);
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   @SubscribeMessage('notify')
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   handleNotify(client: Socket, data: any): void {
-    const user = client.data.user;
-    if (!user) return;
+    try {
+      const user = client.data.user;
+      if (!user) return;
 
-    const socket: any = Array.from(this.server.sockets.values()).find(
-      (socket: Socket) => socket.data.user.id == data.id,
-    );
-    if (!socket) client.emit('error', 'User not found');
-    else {
-      data.id = user.id;
-      socket.emit('notify', data);
+      const socket: any = Array.from(this.server.sockets.values()).find(
+        (socket: Socket) => socket.data.user.id == data.id,
+      );
+      if (!socket) client.emit('error', 'User not found');
+      else {
+        data.id = user.id;
+        socket.emit('notify', data);
+      }
+    } catch (e) {
+      console.error(e);
     }
   }
 }
