b2feba83f81c394c0daacc960114eba3e9784425
Assxios <droge@student.42.fr>
2022-03-24 05:17:27 +0100

    Fix: adding needed routes

--- backend/src/user/controllers/id.controller.ts
+++ backend/src/user/controllers/id.controller.ts
@@ -1,15 +1,45 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Res,
+  StreamableFile,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { Match } from '../entities/match.entity';
 import { User } from '../entities/user.entity';
+import { AvatarService } from '../services/avatar.service';
 import { UserService } from '../services/user.service';
 
 @Controller('user')
 export class IdController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly avatarService: AvatarService,
+  ) {}
+
+  @Get('/')
+  getAllUsers(): Promise<User[]> {
+    return this.userService.getAllUsers();
+  }
 
   @Get('/:id')
   getUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
     return this.userService.getUser(id);
+  }
+
+  @Get('/:id/avatar')
+  async getAvatar(
+    @Param('id', ParseIntPipe) id: number,
+    @Res({ passthrough: true }) response: Response,
+  ): Promise<StreamableFile> {
+    const avatar = await this.userService.getAvatar(id);
+    response.set({
+      'Content-Disposition': `inline; filename="${avatar.filename}"`,
+      'Content-Type': 'image/*',
+    });
+    return this.avatarService.toStreamableFile(avatar.data);
   }
 
   @Get('/matches/:id')
